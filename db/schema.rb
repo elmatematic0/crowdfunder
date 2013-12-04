5ef37a6622ff07ff77c940b567c014c7af2eb724
joshisaurabh <reachme15@yahoo.com>
2013-12-03 19:46:56 -0500

    creating the pledges model and the associations with project and user , tested it in console works fine, milestone towards #7

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131203235115) do
+ActiveRecord::Schema.define(version: 20131204003321) do
+
+  create_table "pledges", force: true do |t|
+    t.integer  "amount"
+    t.integer  "project_id"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "projects", force: true do |t|
     t.string   "title"
