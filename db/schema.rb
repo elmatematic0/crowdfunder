993a1d1f26259631d69163ea5fe89937c717aa2b
joshisaurabh <reachme15@yahoo.com>
2013-12-03 19:24:03 -0500

    Adding user model, created the associations between user and project, updating the view for project to include project owners name and other project details. closes #13

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131203212149) do
+ActiveRecord::Schema.define(version: 20131203235115) do
 
   create_table "projects", force: true do |t|
     t.string   "title"
@@ -20,6 +20,14 @@ ActiveRecord::Schema.define(version: 20131203212149) do
     t.string   "start_date"
     t.string   "end_date"
     t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.text     "bio"
+    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
